ffc7abbb1c2c93116d22dab85986606e1d83bb8d
Mike <mikewrubel51@gmail.com>
2021-10-29 13:27:56 -0400

    commit tree 56c048f59d6893c66e0628337989e5398457e5b5

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(name)
+    if name === nil
+        puts "Squawk!"
+    else
+        puts "Pretty bird!"
+    end
+end
